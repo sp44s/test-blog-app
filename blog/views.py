57c045cef624573c86ef27f55dc2da3587d58343
sp44s <philipshiju@rediffmail.com>
2017-06-23 18:17:37 +0530

    commit finish

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect
 from django.utils import timezone
 from .models import Post
-
+from .forms import Postform
 
 def post_list(request):
     posts = Post.objects.all()
     return render( request, 'blog/post_list.html', {'NamedPosts':posts})
+
+    
+def post_detail(request, pk):
+    post = get_object_or_404(Post,pk=pk)
+    return render(request, 'blog/post_detail.html',{'post':post})
+    
+    
+def post_new(request):
+    if request.method == "POST":
+        form = Postform(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = Postform()
+    return render(request, 'blog/post_edit.html', {'form': form})    
+    
+    
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = Postform(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = Postform(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+    
